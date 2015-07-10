be7669d093c248605e9cea496196fa892cca07e5
Aaron <torch2424@hotmail.com>
2015-07-10 16:09:54 -0700

    Added the photo slider for gifs and screenshots

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,11 @@ angular.module('punchLegacyLandingApp')
   .controller('MainCtrl', function ($scope) {
       //Code goes here
 
-      
+      $scope.myInterval = 3500;
+      $scope.slides = [];
 
+      $scope.slides.push({text: 'cats!', image: '../images/alpha5.gif'});
+      $scope.slides.push({text: 'cats!', image: '../images/alpha5screen1.png'});
+      $scope.slides.push({text: 'cats!', image: '../images/alpha5screen2.png'});
+      $scope.slides.push({text: 'cats!', image: '../images/alpha5screen3.png'});
   });
