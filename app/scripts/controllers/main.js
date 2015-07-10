15f13e2c224ada962789b8f7ee5a92fcd218cb70
Aaron <torch2424@hotmail.com>
2015-07-10 15:13:14 -0700

    Added some screenshots, and finished the header card for the game

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,6 @@
  */
 angular.module('punchLegacyLandingApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      //Code goes here
+
   });
